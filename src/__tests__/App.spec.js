5acac75443413a498a7afc6f4e9ed520d480c6b1
thaothao99 <17521069@gm.uit.edu.vn>
2020-05-04 18:05:57 +0700

    first

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import 'jest-enzyme';
+import App from '../App';
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to React</h2>;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
+  expect(wrapper).toContainReact(welcome);
+});
